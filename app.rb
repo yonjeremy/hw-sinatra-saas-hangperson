93481f5a6913dc3f6c8e54c7acfaab50b7aa1b76
yonjeremy <yonjeremy@gmail.com>
2019-02-04 17:20:36 +0000

    updated views

--- app.rb
+++ app.rb
@@ -39,7 +39,9 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    
+    # @game.guess letter
+
     redirect '/show'
   end
   
@@ -49,8 +51,13 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else 
+      redirect '/show'
+    end
   end
   
   get '/win' do
