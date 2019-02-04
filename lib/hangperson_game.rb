4e488dc054de34b4549f0a05a7c604ab3c2b67d4
yonjeremy <yonjeremy@gmail.com>
2019-02-04 12:38:46 +0000

    finished part 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,10 +7,50 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+  def guess char
+    
+    raise ArgumentError if char == nil or char.empty? or not char.match (/[a-zA-Z]/)
+    
+    char.downcase!
+    
+    if @word.downcase.include? char and not @guesses.include? char
+      @guesses << char
+    elsif not @word.downcase.include? char and not @wrong_guesses.include? char
+      @wrong_guesses << char
+    else
+      return false
+    end
+  end
+  
+  def word_with_guesses
+    return @word.gsub(/./, '-') if @guesses.empty?
+
+    return  @word.gsub(/[^#{@guesses}]/, '-')
+  
+  end
+  
+  def check_win_or_lose
+    
+    if word_with_guesses.include? "-" and wrong_guesses.length > 6
+      return :lose
+    elsif not word_with_guesses.include? "-"
+      return :win
+    else
+      return :play
+    end
+  end
+  
+
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
@@ -25,3 +65,8 @@ class HangpersonGame
   end
 
 end
+
+# h = HangpersonGame.new("abc")
+# h.word("abc")
+# h.guess("a")
+# puts h.guesses()
